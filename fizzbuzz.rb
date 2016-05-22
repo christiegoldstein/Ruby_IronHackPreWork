4c6f6be83d2783ddf39174d9f2f56ce48dbb4669
christiegoldstein <cngold15@gmail.com>
2016-05-21 20:04:43 -0400

    iteration 3

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,37 @@
 number = 1
 
-while number < 101
-    if(number % 3 == 0)
+while number <= 100
+    if(number < 10)
+        number_start = number
+    elsif(number < 100)
+        number_start = number / 10
+    else
+        number_start = number / 100
+    end
+    
+    if(number % 3 == 0 && number % 5 == 0 && number_start == 1)
+        puts "FizzBuzzBang"
+    
+    elsif(number % 3 == 0 && number % 5 == 0)
+        puts "FizzBuzz"
+    
+    elsif(number % 3 == 0 && number_start == 1)
+        puts "FizzBang"
+    
+    elsif(number % 5 == 0 && number_start == 1)
+        puts "BuzzBang"
+    
+    elsif(number % 3 == 0)
         puts "Fizz"
-    end
     
-    if(number % 5 == 0)
+    elsif(number % 5 == 0)
         puts "Buzz"
-    end
+
+    elsif(number_start == 1)
+        puts "Bang"
     
-    if(number % 3 != 0 && number % 5 != 0)
-        number i
+    else
+        puts number
     end
+    number =number + 1
 end
