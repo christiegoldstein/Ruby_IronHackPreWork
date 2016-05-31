368fd810ec76a0d1f0c79401892ca5c982973c50
christiegoldstein <cngold15@gmail.com>
2016-05-31 14:42:40 -0400

    iteration 3 - make default parameter

---
+++ CaesarCipher3.rb
@@ -0,0 +1,30 @@
+def solve_cipher(input , shift = -3)
+    
+    #initialize
+    single_input = input.chars #splits message into array of single characters
+    final_message = ""
+    
+    #iteration
+    single_input.each do |char|
+        
+        if(char == " ")
+            final_message = final_message + " "
+            else
+            char_number = char.ord          #get number value of character
+            
+            if(char_number <= 97)            #wrap from a to z
+                new_number = 123 + shift
+                else
+                new_number = char_number + shift
+            end
+            
+            final_message = final_message + new_number.chr
+        end
+        
+    end
+    
+    return final_message
+    
+end
+
+puts solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
